61d4e22ad8293337f8f4b12a503e78057fd85a8b
Jason Day <jason.day.blue@gmail.com>
2022-02-23 17:28:39 -0600

    updated icon

--- src/App.js
+++ src/App.js
@@ -2,13 +2,7 @@ import "./App.css";
 import PortfolioContainer from "./components/PortfolioContainer";
 
 function App() {
-	return (
-		<div className="App">
-			<header className="App-header">
-				<PortfolioContainer />
-			</header>
-		</div>
-	);
+	return <PortfolioContainer />;
 }
 
 export default App;
